fbd2e2a314dda984a837efe2780b05aa2f96c295
clutc4er <ilia.shenets@gmail.com>
2026-02-20 13:20:50 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ZGŁOSZENIA</title>
+</head>
+
+<body>
+    <div class="header">
+        <h1>Zgłoszenia wydarzeń</h1>
+    </div>
+    <div class="main">
+        <div class="left">
+<h2>Personel</h2>
+<form type="post">
+<input type="checkbox" checked name="Policjant">
+<label for="Policjant">Policjant</label>
+<input type="checkbox" name="Ratownik">
+<label for="Ratownik">Ratownik</label>
+
+</form>
+        </div>
+        <div class="right">
+
+
+        </div>
+    </div>
+    <div class="footer"></div>
+</body>
+
+</html>
